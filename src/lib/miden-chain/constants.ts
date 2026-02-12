a5e5ed15e6a208a90a2c2ffbbab23bec4fd60a19
0xnullifier <utkarsh382004@gmail.com>
2026-02-12 09:07:03 +0530

    chore: prevent the extension in opening in full page when installing

--- src/lib/miden-chain/constants.ts
+++ src/lib/miden-chain/constants.ts
@@ -16,7 +16,7 @@ export enum MIDEN_TRANSPORT_LAYER_NAME {
 
 export const MIDEN_NETWORK_ENDPOINTS = new Map<string, string>([
   [MIDEN_NETWORK_NAME.MAINNET, 'https://api.miden.io'], // Placeholder
-  [MIDEN_NETWORK_NAME.TESTNET, 'https://rpc.testnet.miden.io'],
+  [MIDEN_NETWORK_NAME.TESTNET, 'https://rpc.devnet.miden.io'],
   [MIDEN_NETWORK_NAME.DEVNET, 'https://rpc.devnet.miden.io'],
   [MIDEN_NETWORK_NAME.LOCALNET, 'http://localhost:57291']
 ]);
