40d05934999ef586142d6aeee428f28cf0c474dd
Wiktor Starczewski <poszerny@gmail.com>
2026-03-10 11:35:57 +0100

    feat: add localnet note transport layer endpoint (localhost:57292)

--- src/lib/miden-chain/constants.ts
+++ src/lib/miden-chain/constants.ts
@@ -41,7 +41,7 @@ export const MIDEN_FAUCET_ENDPOINTS = new Map<string, string>([
 
 export const MIDEN_NOTE_TRANSPORT_LAYER_ENDPOINTS = new Map<string, string>([
   [MIDEN_NETWORK_NAME.TESTNET, 'https://transport.miden.io'],
-  [MIDEN_NETWORK_NAME.LOCALNET, 'http://127.0.0.1:57292']
+  [MIDEN_NETWORK_NAME.LOCALNET, 'http://localhost:57292']
 ]);
 
 export const MIDEN_NETWORKS: MidenNetwork[] = [
