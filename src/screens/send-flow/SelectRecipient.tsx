56ab3d24e5a01706b033b66b2e2268a52d615ef5
0xnullifier <utkarsh382004@gmail.com>
2026-07-07 05:28:52 +0530

    fix: select reciepient padding and input placeholder

--- src/screens/send-flow/SelectRecipient.tsx
+++ src/screens/send-flow/SelectRecipient.tsx
@@ -51,7 +51,7 @@ export const SelectRecipient: React.FC<SelectRecipientProps> = ({
   }, [address]);
 
   return (
-    <div className={clsx('flex flex-col h-full min-h-0 bg-app-bg', isMobile() ? 'px-8' : 'px-6')}>
+    <div className={clsx('flex flex-col h-full min-h-0 bg-app-bg px-6')}>
       <div className="flex flex-col flex-1 min-h-0 overflow-y-auto no-scrollbar pt-10">
         <span className="font-heading text-2xl leading-none font-bold text-gray">{t('chooseRecipient')}</span>
 
